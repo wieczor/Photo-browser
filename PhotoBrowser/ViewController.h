a87a509779cd52575115206c9db72c9d74c15448
Lion User <lionuser@Lions-Mac-Pro.local>
2012-02-18 19:47:39 +0100

    Initial Commit

---
+++ PhotoBrowser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoBrowser
+//
+//  Created by Lion User on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
